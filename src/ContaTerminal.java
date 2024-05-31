60b198c1e426b2f34ac3eb04163ec2d426660914
Wallyson Gabriel <wallysonwallyson@hotmail.com>
2024-05-31 00:18:33 -0300

    desafio proposto conta banco

---
+++ src/ContaTerminal.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da conta:");
+        int numero = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Por favor, digite o número da agência:");
+        String agencia = sc.nextLine();
+
+        System.out.println("Por favor, informe o nome do cliente:");
+        String nomeCliente = sc.nextLine();
+
+        System.out.println("Por favor, informe o saldo:");
+        while (!sc.hasNextDouble()) {
+            System.out.println("Entrada inválida. Por favor, digite um número válido para o saldo. Exemplo: 100,00");
+            sc.next();
+        }
+        double saldo = sc.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
+
+        sc.close();
+    }
+}
